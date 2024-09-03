f0f1783e95f9547c636ab05d8f95e6d9640a61f1
Mpho <giftedmpho99@gmail.com>
2024-09-03 14:15:30 +0200

    Created and added OAuth page

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import googleLogo from "../assets/7123025_logo_google_g_icon.svg"; // Importing a Google logo image for the "Continue with Google" button
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "@firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
 
 export default function OAuth() {
+  const dispatch = useDispatch();
+  async function handleClick() {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const res = await signInWithPopup(auth, provider);
+      const req = fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: res.user.displayName,
+          email: res.user.email,
+          avatar: res.user.photoURL,
+        }),
+      });
+      const data = await req.json();
+      dispatch(signInSuccess(data));
+    } catch (error) {
+      console.log("Sign in failed", error);
+    }
+  }
+
   return (
     <div className="my-3">
-      <button className="flex justify-center items-center  w-full bg-white border rounded shadow-sm px-2 py-3 text-sm font-medium text-gray-600 hover:bg-gray-50 dark:text-white hover:bg-gray-25 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+      <button
+        onClick={handleClick}
+        type="submit"
+        className="flex justify-center items-center  w-full bg-white border rounded shadow-sm px-2 py-3 text-sm font-medium text-gray-600 hover:bg-gray-50 dark:text-white hover:bg-gray-25 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+      >
         <img src={googleLogo} alt="google G logo" width={25} />
         <span className="text-regular">Sign in with Google</span>
       </button>
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_zUVUsWOoR6Qv8MXN54ak1pQ7BLM6EJc",
+  authDomain: "dippo-4c7fa.firebaseapp.com",
+  projectId: "dippo-4c7fa",
+  storageBucket: "dippo-4c7fa.appspot.com",
+  messagingSenderId: "317102366829",
+  appId: "1:317102366829:web:eae437e4b506d1445bdf64",
+  measurementId: "G-832MXEDPBP",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
